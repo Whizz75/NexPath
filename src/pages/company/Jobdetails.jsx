644391132b656238821e4f8e6edfacc601ba3aa0
mianca691 <miancaqoane@gmail.com>
2025-11-09 22:36:34 +0200

    Ohh lord :(

--- src/pages/company/Jobdetails.jsx
+++ src/pages/company/Jobdetails.jsx
@@ -2,7 +2,7 @@
 export default function JobDetails() {
   return (
     <div className="flex flex-col items-center justify-center h-full text-center text-gray-600">
-      <h1 className="text-2xl font-semibold mb-2">Job Details Page is under construction</h1>
+      <h1 className="text-2xl font-semibold mb-2">Job Details Page is under construction.</h1>
     </div>
   );
 }
